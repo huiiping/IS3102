dd23e63805e8cac37dde47645893811d4b8556f5
Yongming <tanyongming90@gmail.com>
2017-03-02 14:38:22 +0800

    changed the displayName for cust_membership_tiers from id to tier_name

--- src/Model/Table/CustmembershiptiersTable.php
+++ src/Model/Table/CustmembershiptiersTable.php
@@ -35,7 +35,7 @@ class CustMembershipTiersTable extends Table
         parent::initialize($config);
 
         $this->table('cust_membership_tiers');
-        $this->displayField('id');
+        $this->displayField('tier_name');
         $this->primaryKey('id');
 
         $this->addBehavior('Timestamp');
