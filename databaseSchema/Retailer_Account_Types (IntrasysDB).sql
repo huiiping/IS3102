33458adaa6af558b9dca9693a21f184a373a0ff3
HP <hpong_95@hotmail.com>
2017-03-20 22:04:32 +0800

    changes to DB retailer account types

--- databaseSchema/Retailer_Account_Types (IntrasysDB).sql
+++ databaseSchema/Retailer_Account_Types (IntrasysDB).sql
@@ -1,8 +1,8 @@
 INSERT INTO retailer_acc_types (name, num_of_users, num_of_warehouses, num_of_stores, num_of_products)
-VALUES ('Premium (Gold)', 20, 3, 20, 1000);
+VALUES ('Premium (Gold)', 200, 3, 20, 1000);
 
 INSERT INTO retailer_acc_types (name, num_of_users, num_of_warehouses, num_of_stores, num_of_products)
-VALUES ('Standard (Silver)', 10, 2, 10, 500);
+VALUES ('Standard (Silver)', 100, 2, 10, 500);
 
 INSERT INTO retailer_acc_types (name, num_of_users, num_of_warehouses, num_of_stores, num_of_products)
-VALUES ('Basic (Bronze)', 5, 1, 5, 250);
+VALUES ('Basic (Bronze)', 50, 1, 5, 250);
