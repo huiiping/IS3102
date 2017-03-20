bd191633a11d2acd3056f5879d01dc3378ee4ff9
HAZIQ-ROSLI\gailee8282 <haziq.roslisg@gmail.com>
2017-03-20 17:25:52 +0800

    no message

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -40,7 +40,11 @@ class AppView extends View
 
         if ($this->request->action == 'index') {
             $this->loadHelper('Paginator', ['templates' => 'template_paginator']);
-        }else {
+        } else if ($this->request->action == 'add') {
+            $this->loadHelper('Paginator', ['templates' => 'template_form_1_column']);
+        } else if ($this->request->action == 'edit') {
+            $this->loadHelper('Paginator', ['templates' => 'template_form_1_column']);
+        } else if ($this->request->action == 'delete') {
             $this->loadHelper('Paginator', ['templates' => 'template_form_1_column']);
         }
     }
