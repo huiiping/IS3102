4b5a0a2b765ac3dd4b1b20f1c698f701bedac29a
gwentxy <e0011065@u.nus.edu>
2017-03-23 17:34:03 +0800

    edit not null in stock levels

--- src/Model/Table/StockLevelsTable.php
+++ src/Model/Table/StockLevelsTable.php
@@ -44,13 +44,13 @@ class StockLevelsTable extends Table
 
         $this->belongsTo('Locations', [
             'foreignKey' => 'location_id'
-        ]);
+            ]);
         $this->belongsTo('Products', [
             'foreignKey' => 'product_id'
-        ]);
+            ]);
         $this->belongsTo('RetailerEmployees', [
             'foreignKey' => 'retailer_employee_id'
-        ]);
+            ]);
     }
 
     /**
@@ -62,15 +62,17 @@ class StockLevelsTable extends Table
     public function validationDefault(Validator $validator)
     {
         $validator
-            ->integer('id')
-            ->allowEmpty('id', 'create');
+        ->integer('id')
+        ->allowEmpty('id', 'create');
 
         $validator
-            ->integer('threshold')
-            ->allowEmpty('threshold');
+        ->integer('threshold')
+        ->requirePresence('threshold', 'create')
+        ->notEmpty('threshold');
+
 
         $validator
-            ->allowEmpty('status');
+        ->allowEmpty('status');
 
         return $validator;
     }
